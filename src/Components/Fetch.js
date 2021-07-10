8f38eb38ba211f9737bac5f2a66f2d414d1510f9
Prakash Ravichandran <prakash.ravichandran@hashouttech.com>
2021-07-10 23:23:51 +0530

    added first api call using fetch function

---
+++ src/Components/Fetch.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+
+
+async function Call(){
+    let url = 'https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits';
+
+    let response = await fetch(url);
+
+    let commits = await response.json()
+
+    console.log(commits[0].author.login);
+
+
+}
+Call();
+
+async function CallTwo(){
+    let url = "https://jsonplaceholder.typicode.com/posts";
+
+    let response = await fetch(url);
+
+    let myJson = await response.json();
+
+    // let formData = await response.formData();
+
+    console.log(myJson);
+    console.log(typeof (myJson));
+    console.log(Array.isArray(myJson));
+
+    console.log(myJson[0]);
+
+    <FetchComponent user = "hello"/>
+    // console.log(formData);
+}
+
+CallTwo();
+
+
+const FetchComponent = ({user}) => {
+    return (
+        <div>
+            <h1>Fetch Request</h1>
+            <form name="myform">
+                <label htmlFor="name">Name :</label> <br></br>
+                <input type="text" name="firstName" id="name" value={user}/>
+            </form>
+        </div>
+    )
+};
+
+export default FetchComponent;
