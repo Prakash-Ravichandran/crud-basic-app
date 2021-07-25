7110de95da21345f358fd70e6c6a03fe07ec4b43
Prakash Ravichandran <prakash.ravichandran@hashouttech.com>
2021-07-25 17:51:08 +0530

    added get api with read users

---
+++ src/Components/Create.js
@@ -0,0 +1,74 @@
+import React, { useEffect } from 'react';
+import {Link } from 'react-router-dom';
+import axios from 'axios';
+import '../Components/Create.css';
+
+export function Create(){
+    useEffect(() => {
+        axios.get('https://gorest.co.in/public/v1/users')
+        .then((response) =>
+        {
+         console.log(response.data);
+         console.log(response.data.data[0].name);
+         let firstName = document.getElementById('firstName');
+         firstName.value = response.data.data[0].name;
+         let email = document.getElementById('email');
+         email.value = response.data.data[0].email;
+         let id = document.getElementById('id');
+         id.value = response.data.data[0].id;
+         let gender = document.getElementById('gender');
+         gender.value = response.data.data[0].gender;
+
+   
+
+         console.log(response.data.data.length);
+
+            let arr = response.data.data;
+            arr.map((pointer) => {
+
+                let myButton = document.createElement('button');
+                myButton.type = "button";
+                myButton.innerHTML=`delete - ${pointer.name}`;
+                myButton.onclick = function(pointer){
+                      axios.delete('https://gorest.co.in/public/v1/users')
+                }
+                document.body.appendChild(myButton);
+
+                let ele = document.createElement("input");
+                ele.value = pointer.name;
+                ele.id="name"
+                document.body.appendChild(ele);
+
+                let label = document.createElement('label');
+                label.htmlFor="name";
+                label.innerHTML=pointer.id;
+                document.body.appendChild(label);
+                console.log(pointer.name)});
+
+        })
+     })
+   
+    return(
+        <>
+        <h3>Hello i am a create component</h3>
+        <div style={{border: 20}}>
+        <form name="createUser">
+            <fieldset>
+                <legend>GO rest GET API</legend>
+            <label htmlFor="firstName">FirstName :</label>
+            <input type="text" name = "firstName" id="firstName" autoFocus />
+            <label  htmlFor="email">E-Mail :</label>
+            <input type="text" name ="email" id="email"/>
+            <label  htmlFor="id">Id : </label>
+            <input type="number" name ="id" id="id"/>
+            <label  htmlFor="gender">Gender : </label>
+            <input type="text" name ="id" id="gender"/>
+            <button>Request</button>
+            <button >Delete</button>
+            </fieldset>
+        </form>
+        </div>
+        <Link to="/"><button>Back To Home</button></Link>
+        </>
+    )
+}
---
+++ src/Components/Read.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export function Read(){
+    return(
+         <>
+         <h2>I am read component</h2>
+         </>
+    )
+}
+
